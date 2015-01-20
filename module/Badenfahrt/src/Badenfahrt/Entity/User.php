8dd63da7072eb638402f732976de84026f47b545
samuel egger <samuel.egger7@gmail.com>
2015-01-20 19:55:33 +0100

    Updated the user class: Added getter and setter for each field and added new attribute token; phone (schema) set to be nullable.

--- module/Badenfahrt/src/Badenfahrt/Entity/User.php
+++ module/Badenfahrt/src/Badenfahrt/Entity/User.php
@@ -15,6 +15,8 @@ use ZfcUser\Entity\User as ZfcUserEntity;
  */
 class User extends ZfcUserEntity implements ProviderInterface
 {	
+	protected $token;
+
 	protected $firstName;
 	
 	protected $lastName;
@@ -36,6 +38,62 @@ class User extends ZfcUserEntity implements ProviderInterface
     {
         $this->roles = new ArrayCollection();
     }
+	
+	public function getToken(){
+		return $this->token;
+	}
+	
+	public function setToken($token){
+		$this->token = $token;
+	}
+	
+	public function getFirstName(){
+		return $this->firstName;
+	}
+	
+	public function setFirstName($firstName){
+		$this->firstName = $firstName;
+	}
+	
+	public function getLastName(){
+		return $this->lastName;
+	}
+	
+	public function setLastName($lastName){
+		$this->lastName = $lastName;
+	}
+	
+	public function getStreetAndNumber(){
+		return $this->streetAndNumber;
+	}
+	
+	public function setStreetAndNumber($streetAndNumber){
+		$this->streetAndNumber = $streetAndNumber;
+	}
+	
+	public function getPostalCode(){
+		return $this->postalCode;
+	}
+	
+	public function setPostalCode($postalCode){
+		$this->postalCode = $postalCode;
+	}
+	
+	public function getCity(){
+		return $this->city;
+	}
+	
+	public function setCity($city){
+		$this->city = $city;
+	}
+	
+	public function getPhone(){
+		return $this->phone;
+	}
+	
+	public function setPhone($phone){
+		$this->phone = $phone;
+	}
 
     /**
      * Get role.
