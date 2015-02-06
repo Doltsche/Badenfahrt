d83f21ed5451c63b3c6d1923d6ea0796b2ea087c
samuel egger <samuel.egger7@gmail.com>
2015-02-06 16:03:48 +0100

    Added zend logger for errors

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Zend Framework (http://framework.zend.com/)
  *
@@ -11,14 +12,39 @@ namespace Application;
 
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
+use Zend\Log\Logger;
+use Zend\Log\Writer\Stream;
 
 class Module
 {
+
+    protected $logger;
+
+    public function __construct()
+    {
+        $logger = new Logger;
+        $logger->addWriter(new Stream(__DIR__ . '/../../log/error.log'));
+        
+        Logger::registerErrorHandler($logger);
+    }
+
     public function onBootstrap(MvcEvent $e)
     {
-        $eventManager        = $e->getApplication()->getEventManager();
+        $eventManager = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
+
+        // Handle the dispatch error (exception).
+        $eventManager->attach(\Zend\Mvc\MvcEvent::EVENT_DISPATCH_ERROR, array($this, 'handleError'));
+
+        // Handle the view render error (exception).
+        $eventManager->attach(\Zend\Mvc\MvcEvent::EVENT_RENDER_ERROR, array($this, 'handleError'));
+    }
+
+    public function handleError(MvcEvent $e)
+    {
+        $exception = $e->getParam('exception');
+        $this->logger->log(\Zend\Log\Logger::ERR, $exception->getMessage());
     }
 
     public function getConfig()
@@ -36,4 +62,5 @@ class Module
             ),
         );
     }
+
 }
