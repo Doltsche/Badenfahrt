7ae04ff45fdfd66fdd4324021c79b731e45d5d8e
Andreas Vogelbacher <Andreas.Vogelbacher@gmx.net>
2015-02-08 20:42:27 +0100

    Wrong AES256 key was used that did not match test123

--- scripts/create_test_data.sql
+++ scripts/create_test_data.sql
@@ -29,21 +29,21 @@ SELECT '-- Schreibe Testuser --' AS ' ';
 INSERT INTO user
     (id, identity, 			displayName, 	password, 			 	firstname, 	lastname, 	streetAndNr, 		postalCode, 	city, 		phone, 		state) 
 VALUES
-    (1,	'admin@badenfahrt.ch', 		'Admin', 	'9UDfISFA6RLOuwR3mSUDflExtc17EvfzcYYF6n6BRFI=', 	'Admin', 	'Badenfahrt', 	'Bruggerstrasse 50', 	'5400', 	'Baden', '058585000', 	1),
-    (2,	'samuel.egger7@gmail.com', 	'Samuel Egger', '9UDfISFA6RLOuwR3mSUDflExtc17EvfzcYYF6n6BRFI=', 	'Samuel', 	'Egger', 	'Parkstrasse 1', 	'3072', 	'Ostermundigen', '0794288465', 	1),
-    (3,	'christoph@junker.li', 	'Christoph Junker', '9UDfISFA6RLOuwR3mSUDflExtc17EvfzcYYF6n6BRFI=', 	'Christoph', 	'Junker', 	'Schulhausstrasse 3D', 	'5608', 	'Stetten', '080030300', 	1),
-    (4,	'Andreas.Vogelbacher@gmx.net', 	'Andreas Vogelbacher', '9UDfISFA6RLOuwR3mSUDflExtc17EvfzcYYF6n6BRFI=', 	'Andreas', 	'Vogelbacher', 'Chäppelistrasse 67', 	'5312', 	'Döttingen', '0585858128', 	1);
+    (1,	'admin@badenfahrt.ch', 		'Admin', 	'BTxbiYoGWoab512d8cgNgW9BdiowUOtjaJ3K/wQ4Y1U=', 	'Admin', 	'Badenfahrt', 	'Bruggerstrasse 50', 	'5400', 	'Baden', '058585000', 	1),
+    (2,	'samuel.egger7@gmail.com', 	'Samuel Egger', 'BTxbiYoGWoab512d8cgNgW9BdiowUOtjaJ3K/wQ4Y1U=', 	'Samuel', 	'Egger', 	'Parkstrasse 1', 	'3072', 	'Ostermundigen', '0794288465', 	1),
+    (3,	'christoph@junker.li', 	'Christoph Junker', 'BTxbiYoGWoab512d8cgNgW9BdiowUOtjaJ3K/wQ4Y1U=', 	'Christoph', 	'Junker', 	'Schulhausstrasse 3D', 	'5608', 	'Stetten', '080030300', 	1),
+    (4,	'Andreas.Vogelbacher@gmx.net', 	'Andreas Vogelbacher', 'BTxbiYoGWoab512d8cgNgW9BdiowUOtjaJ3K/wQ4Y1U=', 	'Andreas', 	'Vogelbacher', 'Chäppelistrasse 67', 	'5312', 	'Döttingen', '0585858128', 	1);
 
 INSERT INTO user
     (id, identity, 			displayName, 	password, 			 	firstname, 	lastname, 	streetAndNr, 		postalCode, 	city, 		phone, 		state) 
 VALUES
-    (5,	'test1@123.ch', 		'test1', 	'9UDfISFA6RLOuwR3mSUDflExtc17EvfzcYYF6n6BRFI=', 	'Test', 	'User', 	'Gugus 10', 	'5400', 	'Baden', '058585000', 	1),
-    (6,	'test2@123.ch', 		'test2', 	'9UDfISFA6RLOuwR3mSUDflExtc17EvfzcYYF6n6BRFI=', 	'Test', 	'User', 	'Gugus 20', 	'5400', 	'Baden', '058585000', 	1),
-    (7,	'test3@123.ch', 		'test3', 	'9UDfISFA6RLOuwR3mSUDflExtc17EvfzcYYF6n6BRFI=', 	'Test', 	'User', 	'Gugus 30', 	'5400', 	'Baden', '058585000', 	1),
-    (8,	'test4@123.ch', 		'test4', 	'9UDfISFA6RLOuwR3mSUDflExtc17EvfzcYYF6n6BRFI=', 	'Test', 	'User', 	'Gugus 40', 	'5400', 	'Baden', '058585000', 	1),
-    (9,	'test5@123.ch', 		'test5', 	'9UDfISFA6RLOuwR3mSUDflExtc17EvfzcYYF6n6BRFI=', 	'Test', 	'User', 	'Gugus 50', 	'5400', 	'Baden', '058585000', 	1),
-    (10,	'test6@123.ch', 		'test6', 	'9UDfISFA6RLOuwR3mSUDflExtc17EvfzcYYF6n6BRFI=', 	'Test', 	'User', 	'Gugus 60', 	'5400', 	'Baden', '058585000', 	1),
-    (11,	'test7@123.ch', 		'test7', 	'9UDfISFA6RLOuwR3mSUDflExtc17EvfzcYYF6n6BRFI=', 	'Test', 	'User', 	'Gugus 70', 	'5400', 	'Baden', '058585000', 	1);
+    (5,	'test1@123.ch', 		'test1', 	'BTxbiYoGWoab512d8cgNgW9BdiowUOtjaJ3K/wQ4Y1U=', 	'Test', 	'User', 	'Gugus 10', 	'5400', 	'Baden', '058585000', 	1),
+    (6,	'test2@123.ch', 		'test2', 	'BTxbiYoGWoab512d8cgNgW9BdiowUOtjaJ3K/wQ4Y1U=', 	'Test', 	'User', 	'Gugus 20', 	'5400', 	'Baden', '058585000', 	1),
+    (7,	'test3@123.ch', 		'test3', 	'BTxbiYoGWoab512d8cgNgW9BdiowUOtjaJ3K/wQ4Y1U=', 	'Test', 	'User', 	'Gugus 30', 	'5400', 	'Baden', '058585000', 	1),
+    (8,	'test4@123.ch', 		'test4', 	'BTxbiYoGWoab512d8cgNgW9BdiowUOtjaJ3K/wQ4Y1U=', 	'Test', 	'User', 	'Gugus 40', 	'5400', 	'Baden', '058585000', 	1),
+    (9,	'test5@123.ch', 		'test5', 	'BTxbiYoGWoab512d8cgNgW9BdiowUOtjaJ3K/wQ4Y1U=', 	'Test', 	'User', 	'Gugus 50', 	'5400', 	'Baden', '058585000', 	1),
+    (10,	'test6@123.ch', 		'test6', 	'BTxbiYoGWoab512d8cgNgW9BdiowUOtjaJ3K/wQ4Y1U=', 	'Test', 	'User', 	'Gugus 60', 	'5400', 	'Baden', '058585000', 	1),
+    (11,	'test7@123.ch', 		'test7', 	'BTxbiYoGWoab512d8cgNgW9BdiowUOtjaJ3K/wQ4Y1U=', 	'Test', 	'User', 	'Gugus 70', 	'5400', 	'Baden', '058585000', 	1);
     
    
 
