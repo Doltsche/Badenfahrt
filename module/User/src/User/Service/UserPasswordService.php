81a2792d2957966fd930823528a2b5f2bac081a0
Andreas Vogelbacher <Andreas.Vogelbacher@gmx.net>
2015-02-08 17:58:02 +0100

    Changed algorithm from MD5 to AES in Service/UserPasswordService.php

--- module/User/src/User/Service/UserPasswordService.php
+++ module/User/src/User/Service/UserPasswordService.php
@@ -5,22 +5,41 @@ namespace User\Service;
 /**
  * Implements the UserPasswordServiceInterface interface.
  */
-class UserPasswordService implements UserPasswordServiceInterface
-{
+class UserPasswordService implements UserPasswordServiceInterface {
 
     /**
      * @var \User\Mapper\UserMapperInterface 
      */
     protected $userMapper;
 
+    
+    /**
+     * @var Passkey for AES128 Encryption 
+     */
+    protected $key;
+    
+    /**
+     * Function to encrypt with AES128
+     * 
+     * @param $toEncrypt    password to be encyrpted
+     * @param $key          public key to use for encryption
+     */
+    private function encyptAES128($toEncrypt, $key) {
+        //Encrypt
+        $passcrypt = trim(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, trim($toEncrypt), MCRYPT_MODE_ECB));
+        //Make ist usable for MySQL
+        $encoded = base64_encode($passcrypt);
+        return $encoded;
+    }
+
     /**
      * Creates a new instance of the UserPasswordService class.
      * 
      * @param User\Mapper\UserMapperInterface $userMapper
      */
-    public function __construct($userMapper)
-    {
+    public function __construct($userMapper) {
         $this->userMapper = $userMapper;
+        $this->key ="BadenFahrtPasskey2015";
     }
 
     /**
@@ -29,10 +48,9 @@ class UserPasswordService implements UserPasswordServiceInterface
      * @param \User\Model\User $user
      * @param string $plainPassword
      */
-    public function updatePassword($user, $plainPassword)
-    {
+    public function updatePassword($user, $plainPassword) {
 
-        $encryptedPassword = md5($plainPassword);
+        $encryptedPassword = $this->encyptAES128($plainPassword,$this->key);
         $user->setPassword($encryptedPassword);
 
         return $user;
@@ -45,10 +63,9 @@ class UserPasswordService implements UserPasswordServiceInterface
      * @param \User\Model\User $user
      * @param string $plainPassword
      */
-    public function isSatisfied($user, $plainPassword)
-    {
+    public function isSatisfied($user, $plainPassword) {
         // Encrypt pwd
-        $encryptedPassword = md5($plainPassword);
+        $encryptedPassword = $this->encyptAES128($plainPassword,$this->key);
 
         return $user->getPassword() == $encryptedPassword;
     }
